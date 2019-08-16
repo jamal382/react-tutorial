2a439f721fb8d2f8dbc629429413de19a579c168
jamal382 <jamal.brotecs@gmail.com>
2019-08-16 16:21:40 +0600

    Create You First React Component

---
+++ src/components/First/First.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class First extends Component{
+    render(){
+        return(
+            <h1>I am a Brand new Component</h1>
+        )
+    }
+}
+
+export default First
