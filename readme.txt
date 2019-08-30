cac75ee1e4d78defdfa64b37bfc9759442452387
jamal382 <jamal.brotecs@gmail.com>
2019-08-30 23:00:59 +0600

    Fetch Data From Server

--- readme.txt
+++ readme.txt
@@ -1,2 +1,3 @@
 npm install react-bootstrap bootstrap
 npm install font-awesome
+npm i axiox
--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 import './App.css'
+import Axios from 'axios';
 class App extends Component {
   state = {
+    posts:[]
   }
- 
+
+ componentDidMount(){
+   Axios.get("https://jsonplaceholder.typicode.com/posts")
+   .then(res =>{
+     this.setState({
+       posts: res.data
+     })
+   }).catch(err => console.log(err))
+ }
   render() {
-   
+    //  console.log(this.state)
+   let {posts} = this.state
     return (
-      <div className="App">
-   
+      <div className="container">
+        <ul className="list-group">
+          {posts.map(post => <li key={post.id} className="list-group-item">{post.title}</li>)}
+        </ul>
       </div>
     );
   }
